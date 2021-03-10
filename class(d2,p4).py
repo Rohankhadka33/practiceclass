314a432542e81ad12d8181bc07b286dfbe1c4936
Dell <rohankhadka33@gmail.com>
2021-03-10 10:07:54 +0545

    weight converter

--- class(d2,p4).py
+++ class(d2,p4).py
@@ -1,1 +1,12 @@
-Print("This is ")
+print("This is the weight converter programmed by Mr. Rohan Khadka")
+choice= input("Enter  your choice (Kgs or Lbs)")
+if choice=="Kgs":
+    weight_in_Kgs= float(input("Enter your weight"))
+    pound= weight_in_Kgs*2.205
+    print(f"your weight is {pound} lbs")
+elif choice=="Lbs":
+    weight_in_Lbs= float(input("Enter your weight"))
+    kgs= weight_in_Lbs*0.453
+    print(f"your weight is{kgs} Lbs")
+else:
+    print("Invalid  input")
